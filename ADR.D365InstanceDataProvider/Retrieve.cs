05c8e9ea2db30c01e2227ed8ac2f88dab96c1fe2
Adrian Sayasane <adrian.sayasane@gmail.com>
2018-01-23 22:45:21 +1100

    Initial Commit

--- ADR.D365InstanceDataProvider/Retrieve.cs
+++ ADR.D365InstanceDataProvider/Retrieve.cs
@@ -10,10 +10,23 @@ using System.Threading.Tasks;
 
 namespace ADR.D365InstanceDataProvider
 {
-    public class RetrieveMultiple : IPlugin
+
+    public class Retrieve : IPlugin
     {
+        #region Secure/Unsecure Configuration Setup
+        private string _secureConfig = null;
+        private string _unsecureConfig = null;
+
+        public Retrieve(string unsecureConfig, string secureConfig)
+        {
+            _secureConfig = secureConfig;
+            _unsecureConfig = unsecureConfig;
+        }
+        #endregion
+
         public void Execute(IServiceProvider serviceProvider)
         {
+
             ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
