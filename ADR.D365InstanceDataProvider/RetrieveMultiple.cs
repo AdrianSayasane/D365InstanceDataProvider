970f1d94c1e38b970ec9dea4456395946ab893ac
Adrian Sayasane <adrian.sayasane@gmail.com>
2018-01-23 22:44:25 +1100

    Initial start of project

---
+++ ADR.D365InstanceDataProvider/RetrieveMultiple.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Data.Exceptions;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADR.D365InstanceDataProvider
+{
+    public class RetrieveMultiple : IPlugin
+    {
+        ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+        IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+        IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+        IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
